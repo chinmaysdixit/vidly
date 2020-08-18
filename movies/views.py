4301959f480adb18069ada1c4cb6c998bc68cfbd
chinmaydixit <dixitchinmay94@gmail.com>
2020-08-18 20:13:14 +0530

    Added templates

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+
+    return render(request, 'movies/index.html', {'movies': movies})
